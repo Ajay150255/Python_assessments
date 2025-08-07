b201877c66dbcae9a72e94459a5b4be56a4f3a1a
Ajay150255 <150255@ust.com>
2025-08-07 12:35:39 +0530

    Update Given two strings, the task is to check if these two strings are identical(same) or not

--- Strings/Check_if_two_strings_are_same_or_not.py
+++ Strings/Check_if_two_strings_are_same_or_not.py
@@ -1,1 +1,27 @@
+# Input: s1 = "abc", s2 = "abc" 
+# Output: Yes 
 
+string1 = "abc"
+string2 = "abc"
+if string1 == string2:
+  print("Yes")
+else:
+  print("No")
+
+# Input: s1 = "", s2 = "" 
+# Output: Yes 
+string1 = ""
+string2 = ""
+if string1 == string2:
+  print("Yes")
+else:
+  print("No")
+
+# Input: s1 = "GeeksforGeeks", s2 = "Geeks" 
+# Output: No
+s1 = "GeeksforGeeks"
+s2 = "Geeks"
+if s1 == s2:
+  print("Yes")
+else:
+  print("No")
