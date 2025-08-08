f4699470423d376f290b6a79d708648beb755ba3
Ajay150255 <150255@ust.com>
2025-08-08 11:30:55 +0530

    Create Concatenation of Two Strings

---
+++ Strings/Concatenation_of_Two_Strings.py
@@ -0,0 +1,14 @@
+# Input: s1 = “Hello”, s2 = “World”
+# Output: “HelloWorld”
+# Explanation: Joining “Hello” and “World” results in “HelloWorl.
+s1 = "Hello"
+s2 = "World"
+print(s1+s2)
+
+# Input: s1 = “Good”, s2 = “Morning”
+# Output: “GoodMorning”
+# Explanation: Joining “Good” and “Morning” results in “GoodMorning”
+s1 = "Good"
+s2 = "Morning"
+
+print(s1+s2)
