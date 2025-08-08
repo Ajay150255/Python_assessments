a7e6f5500458985219ead0b5cfc32d187de8e4d8
Ajay150255 <150255@ust.com>
2025-08-08 09:01:33 +0530

    Update Insert_a_character_in_String_at_a_Given_Position.py

--- Strings/Insert_a_character_in_String_at_a_Given_Position.py
+++ Strings/Insert_a_character_in_String_at_a_Given_Position.py
@@ -1,1 +1,21 @@
+# Input: s = "Geeks", c = 'A', pos = 3
+# Output: GeeAks
+""""Using index method"""
+s = "Geeks"
+c = "A"
+pos = 3
+print(s[:pos])
+print(s[pos:])
+new_string = s[:pos] + c + s[pos:]
+print(new_string)
 
+# Input: s = "HelloWorld", c = '!', pos = 5
+# Output: Hello!World
+""""Using index method"""
+s = "HelloWorld"
+c = '!'
+pos = 5
+print(s[:pos])
+print(s[pos:])
+new_string = s[:pos] + c + s[pos:]
+print(new_string)
