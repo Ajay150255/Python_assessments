a3e5197cb6650b86559b3d35c6c3126466f4e056
Ajay150255 <150255@ust.com>
2025-08-13 14:52:30 +0530

    Update Check_if_a_list_contains_only_positive_numbers.py

--- List/Basic List Logic & Conditions Questions/Check_if_a_list_contains_only_positive_numbers.py
+++ List/Basic List Logic & Conditions Questions/Check_if_a_list_contains_only_positive_numbers.py
@@ -1,5 +1,9 @@
 # Check if a list contains only positive numbers.
 # Input: [1, 3, 5, 7] → Output: True
+#### Take input from user as space-separated numbers
+li = list(map(int, input("Enter numbers separated by space: ").split()))
+
+
 li = [1,3,5,7]
 is_postive = True
 for i in li:
